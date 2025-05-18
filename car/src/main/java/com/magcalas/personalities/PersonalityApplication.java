4b0d1d5d4f09cb020c665517dcb346ee0f9c6af7
SeanMgcls <magcalassean@gmail.com>
2025-05-19 07:44:59 +0800

    Add files via upload

---
+++ car/src/main/java/com/magcalas/personalities/PersonalityApplication.java
@@ -0,0 +1,12 @@
+package com.magcalas.personalities; // Update the package name
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalityApplication { // Update the class name
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalityApplication.class, args);
+    }
+}
